5be19bfd9c1d7ce81be0c866566bb0de230cbaf5
Renato-Ribeiroo <108376926+Renato-Ribeiroo@users.noreply.github.com>
2022-07-21 14:59:20 -0300

    Criando a classe http

--- Alura-stickers/src/App.java
+++ Alura-stickers/src/App.java
@@ -1,17 +1,7 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpRequest.Builder;
-import java.net.http.HttpResponse.BodyHandler;
-import java.net.http.HttpResponse.BodyHandlers;
-import java.security.cert.URICertStoreParameters;
 import java.util.List;
 import java.util.Map;
-
-import javax.swing.text.html.HTMLEditorKit.Parser;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -19,25 +9,25 @@ public class App {
         // Fazer uma conexão http e bucar os top 250 filmes 
         // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
         String url = "https://api.mocki.io/v2/549a5d8b"; // usando endereço alternativo
-        URI endereco = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response  = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
+
         //System.out.println(body); imprimir a listagem de filmes
         
         // Extair só os dados que interessam(titulo, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
         //Exibir e manipular os dados 
         var geradora = new GeradoraDeFigurinhas();
          for (int i = 0; i < 3; i++) {
 
-            Map<String,String> filme = listaDeFilmes.get(i);
+            Map<String,String> conteudo = listaDeConteudos.get(i);
 
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
+            String urlImagem = conteudo.get("image");
+            String titulo = conteudo.get("title");
     
             InputStream inputStream = new URL(urlImagem).openStream();
             String nomeArquivo = "saida/" + titulo + ".png";
