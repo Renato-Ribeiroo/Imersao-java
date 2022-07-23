913dc5414e68d0fe2788e0a93dc7c4104e97dd3d
Renato-Ribeiroo <108376926+Renato-Ribeiroo@users.noreply.github.com>
2022-07-23 12:47:59 -0300

    Conectando a aplicacao ao mongo DB

--- linguagens-api/src/main/java/br/com/alura/linguagens/api/Linguagem.java
+++ linguagens-api/src/main/java/br/com/alura/linguagens/api/Linguagem.java
@@ -1,9 +1,20 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
 public class Linguagem {
+
+    @Id
+    private String id;
     private String title;
     private String image;
     private int ranking;
+
+    public Linguagem() {
+
+    }
 
     public Linguagem(String title, String image, int ranking) {
         this.title = title;
